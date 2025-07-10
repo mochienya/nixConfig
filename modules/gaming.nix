cc300918ec995fca23b67e0e822c7346921e67f0
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-07-10 23:27:37 +0200

    autoclicker

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -9,4 +9,17 @@
     update.onActivation = true;
     uninstallUnmanaged = true;
   };
+  # autoclicker that supports wayland from github
+  environment.systemPackages = [(pkgs.rustPlatform.buildRustPackage rec {
+    pname = "theclicker";
+    version = "0.2.3";
+    cargoHash = "sha256-JL/X2s/SnmK88btz/MmB6t8nKqUXks07+tWXc4trfLM=";
+    # getting these hashes was by far the worst part of doing this
+    src = pkgs.fetchFromGitHub {
+      owner = "konkitoman";
+      repo = "autoclicker";
+      rev = version;
+      hash = "sha256-Q5Uwl2SWdat/cHRPf4GVQihn1NwlFKbkpWRFnScnvw0=";
+    };
+  })];
 }
