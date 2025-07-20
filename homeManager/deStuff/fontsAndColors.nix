305ed5506e58736f9c4ece638de302f3e665630b
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-07-20 21:41:53 +0200

    nunito :3

--- homeManager/deStuff/fontsAndColors.nix
+++ homeManager/deStuff/fontsAndColors.nix
@@ -6,8 +6,12 @@
   home.file."/.local/share/fonts/Mochie_Iosevka/MochieIosevka-Regular.ttf".source =
     ../../files/MochieIosevka-Regular.ttf;
 
+  home.file."/.local/share/fonts/Nunito/Nunito-VariableFont_wght.ttf".source =
+    ../../files/Nunito-VariableFont_wght.ttf;
+
   home.packages = with pkgs; [
     nerd-fonts.symbols-only
+    texlivePackages.nunito
   ];
 
 }
