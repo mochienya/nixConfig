37926281853d5237d2bc5c46465cf987800703cb
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-08-31 13:45:15 +0200

    new iosevka with weights and italic

--- homeManager/deStuff/fontsAndColors.nix
+++ homeManager/deStuff/fontsAndColors.nix
@@ -1,18 +1,29 @@
 { pkgs, ... }:
+let
 
+  mochieIosevka = pkgs.stdenv.mkDerivation {
+    name = "MochieIosevka";
+    version = "1.0";
+
+    src = ../../files;
+
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype
+      cp -R ./MochieIosevka $out/share/fonts/truetype/MochieIosevka/
+    '';
+  };
+in
 {
   # awesome font!!
   fonts.fontconfig.enable = true;
-  home.file."/.local/share/fonts/Mochie_Iosevka/MochieIosevka-Regular.ttf".source =
-    ../../files/MochieIosevka-Regular.ttf;
 
-  home.file."/.local/share/fonts/Nunito/Nunito-VariableFont_wght.ttf".source =
+  home.file."/.local/share/fonts/truetype/Nunito/Nunito-VariableFont_wght.ttf".source =
     ../../files/Nunito-VariableFont_wght.ttf;
 
   home.packages = with pkgs; [
     nerd-fonts.symbols-only
     twitter-color-emoji
-    texlivePackages.nunito
+    mochieIosevka
   ];
 
 }
