7beedecb36d2d07dfde472168df933b5879e864b
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-09-07 01:27:12 +0200

    stop font being removed on reboot, sensible helix

--- homeManager/cli/helix.nix
+++ homeManager/cli/helix.nix
@@ -16,6 +16,17 @@
         cursor-shape.insert = "bar";
         auto-save.focus-lost = true;
       };
+      keys.normal = {
+        # most of the time i don't care about what i'm deleting
+        # i might delete something while moving around even if i DO care
+        # and using registers is inconvenient
+        "d" = "delete_selection_noyank";
+        "A-d" = "delete_selection";
+        "c" = "change_selection_noyank";
+        "A-c" = "change_selection";
+        # "A-j" = ["select_register m" "delete_selection" "move_visual_line_down" "paste_after"];
+        # "A-k" = ["select_register m" "delete_selection" "move_visual_line_up" "paste_after"];
+      };
     };
   };
 }
