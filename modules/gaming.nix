3aecc9584a9d83f8ddfe9538ad296117a7eaa6fb
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-11-16 00:36:32 +0100

    gaming stuff woa

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -22,29 +22,26 @@
     update.onActivation = true;
     uninstallUnmanaged = true;
   };
-  # autoclicker that supports wayland from github
-  environment.systemPackages = [
-    (pkgs.rustPlatform.buildRustPackage rec {
-      pname = "theclicker";
-      version = "0.2.3";
-      cargoHash = "sha256-JL/X2s/SnmK88btz/MmB6t8nKqUXks07+tWXc4trfLM=";
-      # getting these hashes was by far the worst part of doing this
-      src = pkgs.fetchFromGitHub {
-        owner = "konkitoman";
-        repo = "autoclicker";
-        rev = version;
-        hash = "sha256-Q5Uwl2SWdat/cHRPf4GVQihn1NwlFKbkpWRFnScnvw0=";
-      };
-    })
-  ]
-  ++ (with pkgs; [
+  environment.systemPackages = with pkgs; [
     (lutris.override {
-      extraPkgs = ps: with ps; [
-        umu-launcher
-        gamescope
-        gamemode
-      ];
+      extraPkgs =
+        ps: with ps; [
+          umu-launcher
+          gamescope
+          gamemode
+        ];
     })
     protonup-rs
-  ]);
+    r2modman
+  ];
+
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = -10;
+        inhibit_screensaver = 0;
+      };
+    };
+  };
 }
