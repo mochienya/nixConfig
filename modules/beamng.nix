58fad3d67eae9d73f9c1b5a6a45bede3e4018764
mochie~! <187453775+mochienya@users.noreply.github.com>
2026-01-19 22:37:24 -0700

    beam engive drine
    haha the car crashed into the pole and wrapped around it

---
+++ modules/beamng.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    (
+      let
+        libraryPath =
+          with pkgs;
+          lib.makeLibraryPath [
+            fontconfig
+            freetype
+            glib
+            nss
+            nspr
+            dbus
+            atk
+            cups
+            libdrm
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            libgbm
+            xorg.libxcb
+            libxkbcommon
+            pango
+            cairo
+            alsa-lib
+            vulkan-loader
+            libglvnd
+          ];
+      in
+      pkgs.writeScriptBin "launch-beamng" ''
+        #!/usr/bin/env ${pkgs.lib.getExe pkgs.bash}
+
+        set -euo pipefail
+
+        export LD_LIBRARY_PATH="${libraryPath}:$LD_LIBRARY_PATH"
+
+        cmd=$(printf '%s ' "$@")
+        cmd=$(sed -E 's#/[^ ]*/steam-launch-wrapper -- .* ([^ ]*/proton waitforexitandrun)##' <<< "$cmd")
+        cmd=$(sed -E 's#BeamNG\.drive\.exe#BinLinux/BeamNG.drive.x64#' <<< "$cmd")
+
+        $cmd
+      ''
+    )
+  ];
+}
